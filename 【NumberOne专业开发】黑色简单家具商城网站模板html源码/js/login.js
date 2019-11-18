89a45ca3436a29284db34939f41cc35c7b1cfd6f
huangyangshan <yhuang15@mail.sfsu.edu>
2019-11-18 16:37:07 +0800

    first commit

---
+++ 【NumberOne专业开发】黑色简单家具商城网站模板html源码/js/login.js
@@ -0,0 +1,85 @@
+// JavaScript Document
+$(document).ready(function(){
+						   		   
+	
+	$('a.poplight[href^=#]').click(function() {
+		var popID = $(this).attr('rel'); 
+		var popURL = $(this).attr('href');
+				
+	
+		var query= popURL.split('?');
+		var dim= query[1].split('&');
+		var popWidth = dim[0].split('=')[1]; 
+
+		
+		$('#' + popID).fadeIn().css({ 'width': Number( popWidth ) }).prepend('');
+		
+	
+		var popMargTop = ($('#' + popID).height() + 80) / 2;
+		var popMargLeft = ($('#' + popID).width() + 80) / 2;
+		
+	
+		$('#' + popID).css({ 
+			'margin-top' : -popMargTop,
+			'margin-left' : -popMargLeft
+		});
+		
+		$('body').append('<div id="fade"></div>'); 
+		$('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn(); 
+		
+		return false;
+	});
+	
+	
+
+	$('a.close, #fade').live('click', function() { 
+	  	$('#fade , .popup_block').fadeOut(function() {
+			$('#fade, a.close').remove();  
+	}); 
+		
+		return false;
+	});
+	
+	
+	//
+	
+	$('a.poplight2[href^=#]').click(function() {
+		var popID = $(this).attr('rel'); 
+		var popURL =$(this).attr('href');
+				
+	
+		var query= popURL.split('?');
+		var dim= query[1].split('&');
+		var popWidth = dim[0].split('=')[1]; 
+
+		
+		$('#' + popID).fadeIn().css({ 'width': Number( popWidth ) }).prepend('');
+		
+	
+		var popMargTop = ($('#' + popID).height() + 80) / 2;
+		var popMargLeft = ($('#' + popID).width() + 80) / 2;
+		
+	
+		$('#' + popID).css({ 
+			'margin-top' : -popMargTop,
+			'margin-left' : -popMargLeft
+		});
+		
+		$('body').append('<div id="fade"></div>'); 
+		$('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn(); 
+		
+		return false;
+	});
+	
+	
+
+	$('a.close, #fade').live('click', function() { 
+	  	$('#fade , .popup_block2').fadeOut(function() {
+			$('#fade, a.close').remove();  
+	}); 
+		
+		return false;
+	});
+
+	
+});
---
+++ 【NumberOne专业开发】黑色简单家具商城网站模板html源码/js/sellImg.js
@@ -0,0 +1,67 @@
+// JavaScript Document
+/*163css*/
+ph$ = {
+	bindEvt : function() {
+		var o = {};
+		o.btn_sell_l = $("#pageLeft");
+		if (o.btn_sell_l.length > 0) {
+			o.btn_sell_l.bind("click", function() {
+				ph$.scrollLveSell('right');
+			});
+		}
+		o.btn_sell_r = $("#pageRight");
+		if (o.btn_sell_r.length > 0) {
+			o.btn_sell_r.bind("click", function() {
+				ph$.scrollLveSell('left');
+			});
+		}
+
+	},
+	scrollLveSell : function(o) {
+		if (this.cfg.lv_flag == 0)
+			return true;
+		var tar = $("#sell_order");
+		var tar2 = $("#sell_order_t");
+		tar.stop();
+		tar2.stop();
+		var max_num = $("#sell_order>li").length;
+		var width = 126;
+		var left = 0;
+		var pos = tar.position();
+		//alert(pos.left + "|" + width);
+		if (o == "left") {
+			if (Math.abs(pos.left)+width > width*(max_num-4)) {
+				return false;
+			}
+			left = pos.left - width;
+		} else if (o == "right") {
+			if (pos.left >= 0) {
+				return false;
+			}
+			left = pos.left + width;
+		}
+		left = left + "px";
+		//alert(left);
+		ph$.cfg.lv_flag = 0;
+		tar.animate({
+			left: left
+		}, 600);
+		tar2.animate({
+			left: left
+		}, 600, function() {
+			ph$.cfg.lv_flag = 1;
+			//alert(pos.left);
+		});
+	},
+
+	cfg : {
+
+	},
+	init : function() {
+		this.bindEvt();
+	}
+};
+
+jQuery(function($) {
+	ph$.init();
+});
